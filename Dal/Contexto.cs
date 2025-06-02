2434fa69c195a5af9cc9bcc7de911f125f1bec67
Danielvi <danielvilopez@gmail.com>
2025-06-02 19:56:45 -0400

    Configura la clase contexto

--- Dal/Contexto.cs
+++ Dal/Contexto.cs
@@ -1,6 +1,13 @@
-﻿namespace Ramon_Lopez_AP1_P1.Dal;
+﻿using Microsoft.EntityFrameworkCore;
+using Ramon_Lopez_AP1_P1.Models;
 
-public class Contexto
+namespace Ramon_Lopez_AP1_P1.Dal;
+
+public class Contexto : DbContext
 {
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
+    public DbSet<Registro> Registros { get; set; }
+
+    public Contexto() { }
 }
