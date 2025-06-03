ae81bfab515fd464a761f0527241f68d8ba551ca
Danielvi <danielvilopez@gmail.com>
2025-06-02 20:06:49 -0400

    Crea la llave primaria en el Modelo

--- Models/Registro.cs
+++ Models/Registro.cs
@@ -1,6 +1,9 @@
-﻿namespace Ramon_Lopez_AP1_P1.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ramon_Lopez_AP1_P1.Models;
 
 public class Registro
 {
-
+    [Key]
+    public int RegistroId { get; set; }
 }
