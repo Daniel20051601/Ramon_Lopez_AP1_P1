6b668f975da06df2c25be0fc4037cf878f6a7078
Danielvi <danielvilopez@gmail.com>
2025-06-09 18:56:01 -0400

    Cambia el nombre del DbSet

--- Dal/Contexto.cs
+++ Dal/Contexto.cs
@@ -7,7 +7,7 @@ public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
-    public DbSet<Aportes> Registros { get; set; }
+    public DbSet<Aportes> Aportes { get; set; }
 
     public Contexto() { }
 }
